f5a41fd282b746be632fd13522e58bf02cfec5fa
Ismat <ismetsemedov@gmail.com>
2025-01-25 15:31:09 +0400

    check

--- main.py
+++ main.py
@@ -479,16 +479,16 @@ async def run_scrapers():
             return []
             
         scrapers = [
-            ("Arenda.az", OptimizedArendaScraper()),
-            ("EV10.az", EV10Scraper()),
-            ("YeniEmlak.az", YeniEmlakScraper()),
-            ("Emlak.az", EmlakAzScraper()),
+            # ("Arenda.az", OptimizedArendaScraper()),
+            # ("EV10.az", EV10Scraper()),
+            # ("YeniEmlak.az", YeniEmlakScraper()),
+            # ("Emlak.az", EmlakAzScraper()),
             ("Bina.az", BinaScraper()),
-            ("Ipoteka.az", IpotekaScraper()),
-            ("Unvan.az", UnvanScraper()),
-            ("VipEmlak.az", VipEmlakScraper()),
-            ("Lalafo.az", LalafoScraper()),
-            ("Tap.az", TapAzScraper())
+            # ("Ipoteka.az", IpotekaScraper()),
+            # ("Unvan.az", UnvanScraper()),
+            # ("VipEmlak.az", VipEmlakScraper()),
+            # ("Lalafo.az", LalafoScraper()),
+            # ("Tap.az", TapAzScraper())
         ]
         
         for name, scraper in scrapers:
