6e3b08f1ead02a806ddd268b69620d68b2317635
Ismat <ismetsemedov@gmail.com>
2025-01-31 22:07:29 +0400

    20 page X2

--- main.py
+++ main.py
@@ -535,7 +535,7 @@ async def run_scrapers():
     
     # Standardize website names
     page_config = {
-        "bina.az": 10,
+        "bina.az": 3,
         # "tap.az": 4,        
         # "emlak.az": 4,      
         # "lalafo.az": 4,     
